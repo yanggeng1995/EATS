53aeb0494f919c4c9cbd9d29c0bd2d7573af306c
yanggeng <yanggeng1995@gmail.com>
2020-06-28 09:30:36 +0800

    update

--- models/encoder.py
+++ models/encoder.py
@@ -4,41 +4,37 @@ import torch.nn.functional as F
 from torch.nn.utils import spectral_norm
 from modules import Conv1d, Linear
 
-
 class Encoder(nn.Module):
     def __init__(self,
            encoder_dim,
-           z_channels,
-           s_channels):
+           spk_embedding_dim,
+           z_dim):
         super(Encoder, self).__init__()
 
         self.encoder_dim = encoder_dim
-        self.z_channels = z_channels
-        self.s_channels = s_channels
+        self.spk_embedding_dim = spk_embedding_dim
+        self.z_dim = z_dim
 
-        self.aligner = Aligner(encoder_dim, z_channels, s_channels, 10)
-        self.expand_frame_model = ExpandFrame()
+        self.aligner = Aligner(encoder_dim, z_dim, spk_embedding_dim)
 
     def forward(self, encoder_inputs, z, speaker_inputs):
         encoder_outputs, duration = self.aligner(encoder_inputs, z, speaker_inputs)
 
-        decoder_inputs = self.expand_frame_model(encoder_outputs, duration).transpose(1, 2)
-
-        return decoder_inputs, duration
+        return encoder_outputs, duration
 
 class Aligner(nn.Module):
     def __init__(self,
-           embed_dim,
+           in_channels,
            z_channels,
            s_channels,
            num_dilation_layer=10):
         super(Aligner, self).__init__()
 
-        self.embed_dim = embed_dim
+        self.in_channels = in_channels
         self.z_channels = z_channels
         self.s_channels = s_channels
 
-        self.pre_process = Conv1d(embed_dim, 256, kernel_size=3)
+        self.pre_process = Conv1d(in_channels, 256, kernel_size=3)
 
         self.dilated_conv_layers = nn.ModuleList()
         for i in range(num_dilation_layer):
@@ -61,37 +57,7 @@ class Aligner(nn.Module):
         encoder_outputs = outputs.transpose(1, 2)
         duration = self.post_process(outputs.transpose(1, 2))
 
-        return encoder_outputs, duration
-
-class ExpandFrame(nn.Module):
-    def __init__(self):
-        super(ExpandFrame, self).__init__()
-
-    def forward(self, encoder_outputs, duration):
-        duration = (duration + 1.5).long()
-        batch_size = encoder_outputs.size(0)
-
-        lists = []
-        for i in range(batch_size):
-            lists.append(self.one_batch_expand(encoder_outputs[i], duration[i]))
-        outputs = torch.stack(lists, dim=0)
-
-        return outputs
-
-    def one_batch_expand(self, encoder_outputs, duration):
-        num_phoneme = encoder_outputs.size(0)
-        sums = torch.sum(duration, dim=0, keepdim=True).repeat(num_phoneme, 1)
-        center = (sums - (0.5 * duration)).float()
-        lists = []
-        for t in range(sums[0, 0]):
-            for n in range(num_phoneme):
-                x = torch.exp(-0.1 * (t - center[n : n + 1])**2)
-                y = torch.sum(torch.exp(-0.1 * (t - center)**2), dim=0)
-                w_n_t = x / y
-            lists.append(torch.sum(w_n_t * encoder_outputs, dim=0))
-        output = torch.stack(lists, dim=0)
-
-        return output
+        return encoder_outputs, duration.squeeze(-1)
 
 class DilatedConvBlock(nn.Module):
 
@@ -156,9 +122,9 @@ class BatchNorm1dLayer(nn.Module):
       return outputs
 
 if __name__ == "__main__":
-    model = Encoder(256, 128, 128)
-    phoneme_inputs = torch.LongTensor([0, 1]).view(2, 1).repeat(1, 10)
-    z = torch.randn(2, 128)
-    speaker = torch.LongTensor([0, 1]).view(2, 1)
-    outputs, duration = model(phoneme_inputs, z, speaker)
-    print(outputs.shape)
+    model = Encoder(256, 64, 64)
+    encoder_inputs = torch.randn(2, 256, 10)
+    z = torch.randn(2, 64)
+    speaker = torch.randn(1, 64)
+    outputs, duration = model(encoder_inputs, z, speaker)
+    print(outputs.shape, duration.shape)
--- models/model.py
+++ models/model.py
@@ -5,37 +5,84 @@ from decoder import Decoder
 
 class EETS(nn.Module):
     def __init__(self,
-           encoder_dim,
-           z_channels=128,
-           s_channels=128):
+           phone_embedding_dim=128,
+           tone_embedding_dim=64,
+           prosody_embedding_dim=32,
+           seg_embedding_dim=32,
+           spk_embedding_dim=64,
+           z_dim=64):
         super(EETS, self).__init__()
 
-        self.encoder_dim = encoder_dim
-        self.z_channels = z_channels
-        self.s_channels = s_channels
+        self.phone_embedding_dim = phone_embedding_dim
+        self.tone_embedding_dim = tone_embedding_dim
+        self.prosody_embedding_dim = prosody_embedding_dim
+        self.seg_embedding_dim = seg_embedding_dim
 
-        self.phoneme_embedding = nn.Embedding(1000, encoder_dim)
-        self.speaker_embedding = nn.Embedding(100, s_channels)
+        self.spk_embedding_dim = spk_embedding_dim
+        self.z_dim = z_dim
 
-        self.encoder = Encoder(encoder_dim, z_channels, s_channels)
-        self.decoder = Decoder(256, z_channels + s_channels)
+        self.encoder_dim = phone_embedding_dim + tone_embedding_dim + prosody_embedding_dim + seg_embedding_dim
 
-    def forward(self, inputs, z, speakers):
-        encoder_inputs = self.phoneme_embedding(inputs).transpose(1, 2)
-        speaker_inputs = self.speaker_embedding(speakers).squeeze(1)
+        self.phone_embed = nn.Embedding(100, phone_embedding_dim)
+        self.tone_embed = nn.Embedding(8, tone_embedding_dim)
+        self.prosody_embed = nn.Embedding(4, prosody_embedding_dim)
+        self.seg_embed = nn.Embedding(4, seg_embedding_dim)
+        self.speaker_embed = nn.Embedding(100, spk_embedding_dim)
 
-        decoder_inputs, duration = self.encoder(encoder_inputs, z, speaker_inputs)
+        self.encoder = Encoder(self.encoder_dim, spk_embedding_dim, z_dim)
+        self.expand_model = ExpandFrame()
+        self.decoder = Decoder(256, spk_embedding_dim + z_dim)
 
-        z_inputs = torch.cat([z, speaker_inputs], dim=1)
-        print(decoder_inputs.shape, z_inputs.shape)
-        outputs = self.decoder(decoder_inputs, z_inputs)
+    def forward(self, phone, tone, prosody, segment, noise, speakers, duration):
+        phone_inputs = self.phone_embed(phone).transpose(1, 2)
+        tone_inputs = self.tone_embed(tone).transpose(1, 2)
+        prosody_inputs = self.prosody_embed(prosody).transpose(1, 2)
+        segment_inputs = self.seg_embed(segment).transpose(1, 2)
 
-        return outputs, duration
+        encoder_inputs = torch.cat([phone_inputs, tone_inputs, prosody_inputs, segment_inputs], dim=1)
+
+        speaker_inputs = self.speaker_embed(speakers).squeeze(1)
+
+        encoder_outputs, predict_duration = self.encoder(encoder_inputs, noise, speaker_inputs)
+
+        decoder_inputs = self.expand_model(encoder_outputs, duration)
+        
+        z_inputs = torch.cat([noise, speaker_inputs], dim=1)
+        outputs = self.decoder(decoder_inputs.transpose(1, 2), z_inputs)
+
+        return outputs, predict_duration
+
+class ExpandFrame(nn.Module):
+    def __init__(self):
+        super(ExpandFrame, self).__init__()
+        pass
+
+    def forward(self, encoder_outputs, duration):
+        t = torch.round(torch.sum(duration, dim=-1, keepdim=True)) #[B, 1]
+        e = torch.cumsum(duration, dim=-1).float() #[B, L]
+        c = e - 0.5 * t #[B, L]
+
+        t = torch.range(0, torch.max(t)) 
+        t = t.unsqueeze(0).unsqueeze(1) #[1, 1, T]
+        c = c.unsqueeze(2)
+        w_1 = torch.exp(-0.1 * (t - c) ** 2)  # [B, L, T]
+        w_2 = torch.sum(torch.exp(-0.1 * (t - c) ** 2), dim=1, keepdim=True)  # [B, 1, T]
+        
+        w = w_1 / w_2
+
+        out = torch.matmul(w.transpose(1, 2), encoder_outputs)
+
+        return out
 
 if __name__ == "__main__":
-    model = EETS(256, 128, 128)
-    phoneme_inputs = torch.LongTensor([0, 1]).view(2, 1).repeat(1, 10)
-    z = torch.randn(2, 128)
+    l = [[1.5, 2.3, 3.4, 4.4, 5.1, 4.2, 3.5, 2.6, 1.8, 0, 0, 0, 0],
+     [1.5, 2.3, 3.4, 4.4, 5.1, 4.2, 3.5, 2.6, 1.8, 4.5, 5.5, 2.3, 5.6]]
+    model = EETS()
+    outputs = torch.randn(2, 13, 512)
+    phone = torch.LongTensor([1,0,2,1,0,1,1,1,1,1,1,1,1]).view(1, 13).repeat(2, 1)
     speaker = torch.LongTensor([0, 1]).view(2, 1)
-    outputs, duration = model(phoneme_inputs, z, speaker)
-    print(outputs.shape)
+    duration = torch.FloatTensor(l)
+    noise = torch.randn(2, 64)
+    print(duration.shape)
+    out, pduration= model(phone, phone, phone, phone, noise, speaker, duration)
+    print(out.shape, pduration.shape)
